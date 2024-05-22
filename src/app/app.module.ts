79a999b054be6c3149f1c7054f17053389b04cee
Naruto9306 <pedro.moran93@yahoo.com>
2024-05-22 17:09:37 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReservaComponent } from './reserva/reserva.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { AboutComponent } from './about/about.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { PreciosComponent } from './precios/precios.component';
+import { ContactosComponent } from './contactos/contactos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReservaComponent,
+    LoginComponent,
+    MenuComponent,
+    InicioComponent,
+    AboutComponent,
+    ServiciosComponent,
+    PreciosComponent,
+    ContactosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
